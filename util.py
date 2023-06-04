2d38c91600b0c968db7fc1b6f8f1524f65c5af8a
dmitriy-laptop <dimontura@yandex.ru>
2023-06-04 17:27:04 +0700

    в util добавил декоратор-таймер из 2.6

--- util.py
+++ util.py
@@ -1,4 +1,7 @@
 import asyncio
+from typing import Callable, Any
+import time
+import functools
 
 
 async def delay(delay_seconds: int) -> int:
@@ -6,3 +9,19 @@ async def delay(delay_seconds: int) -> int:
     await asyncio.sleep(delay_seconds)
     print(f'@ Сон в течение {delay_seconds} с закончился')
     return delay_seconds
+
+
+def async_timed() -> Any:
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f"выполняется {func} с аргументами {args} {kwargs}")
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f"{func} завершилась за {total:.4f} с")
+        return wrapped
+    return wrapper
